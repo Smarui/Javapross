af99281f36ba96ace183b37c870b5a8c3089306d
13576044289 <1697827531@111.com>
2022-03-03 15:20:23 +0800

    开篇

---
+++ src/test.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("彭志强是傻狗:");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("是       不是");
+        String s = sc.nextLine();
+    }
+}
